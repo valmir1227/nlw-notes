2e43610791c7e92e0b1afd79a1dc5766f864355e
Valmir Almeida <almeidavalmir76@gmail.com>
2024-02-10 19:15:14 -0300

    Fist commit

---
+++ src/app.tsx
@@ -0,0 +1,32 @@
+import logo from "./assets/logo-nlw-expert.svg";
+import { NewNoteCard } from "./components/new-note-card";
+import { NoteCard } from "./components/note-card";
+
+const note = {
+  date: new Date(),
+  content: "Ola mundo",
+};
+
+export function App() {
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt="nlw expert notes logo" />
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+        />
+      </form>
+
+      <div className="h-px bg-slate-700"></div>
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard />
+        <NoteCard note={note} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
